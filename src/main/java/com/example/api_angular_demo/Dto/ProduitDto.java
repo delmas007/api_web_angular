09a08be7c8b343537b948ee7707420e283744f32
delmas007 <azertyazerty1ze@gmail.com>
2024-01-25 09:32:26 +0000

    Dto

--- src/main/java/com/example/api_angular_demo/Dto/ProduitDto.java
+++ src/main/java/com/example/api_angular_demo/Dto/ProduitDto.java
@@ -1,5 +1,6 @@
 package com.example.api_angular_demo.Dto;
 
+import com.example.api_angular_demo.Model.Produit;
 import jakarta.persistence.Column;
 import lombok.Builder;
 import lombok.Data;
@@ -9,12 +10,32 @@ import lombok.Data;
 public class ProduitDto {
     private Integer id ;
 
-    @Column(name = "name")
     private String name;
 
-    @Column(name = "price")
     private Integer price;
 
-    @Column(name = "checked")
     private Boolean checked;
+
+    public static ProduitDto fromEntity(Produit produit){
+        if(produit==null){
+            return  null;
+        }
+        return ProduitDto.builder()
+                .id(produit.getId())
+                .price(produit.getPrice())
+                .name(produit.getName())
+                .checked(produit.getChecked())
+                .build();
+    }
+
+    public static Produit toEntity(ProduitDto produitDto){
+        if(produitDto == null){
+            return null;
+        }
+        return  Produit.builder()
+                .name(produitDto.getName())
+                .price(produitDto.getPrice())
+                .checked(produitDto.getChecked())
+                .build();
+    }
 }
