4765f9548afa77d7dff02feee278be2a46479741
delmas007 <azertyazerty1ze@gmail.com>
2024-01-25 09:04:18 +0000

    premier commit

---
+++ src/main/java/com/example/api_angular_demo/ApiAngularDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_angular_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAngularDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAngularDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/api_angular_demo/ApiAngularDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_angular_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAngularDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
