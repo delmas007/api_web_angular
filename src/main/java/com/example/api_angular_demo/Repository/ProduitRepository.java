d03af58277c1fd88d01a27d4d753698870b69f1b
delmas007 <azertyazerty1ze@gmail.com>
2024-01-25 10:56:41 +0000

    Service

--- src/main/java/com/example/api_angular_demo/Repository/ProduitRepository.java
+++ src/main/java/com/example/api_angular_demo/Repository/ProduitRepository.java
@@ -2,10 +2,12 @@ package com.example.api_angular_demo.Repository;
 
 import com.example.api_angular_demo.Model.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ProduitRepository extends JpaRepository<Produit,Integer> {
     Optional<Produit> findById(String id);
 
---
+++ src/main/java/com/example/api_angular_demo/Service/Imp/ProduitServiceImp.java
@@ -0,0 +1,44 @@
+package com.example.api_angular_demo.Service.Imp;
+
+import com.example.api_angular_demo.Dto.ProduitDto;
+import com.example.api_angular_demo.Model.Produit;
+import com.example.api_angular_demo.Repository.ProduitRepository;
+import com.example.api_angular_demo.Service.ProduitService;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ProduitServiceImp implements ProduitService {
+
+    ProduitRepository produitRepository;
+
+
+    @Override
+    public ProduitDto AjouterProduit(ProduitDto produitDto) {
+        Produit donnee = Produit.builder()
+                .name(produitDto.getName())
+                .checked(produitDto.getChecked())
+                .price(produitDto.getPrice())
+                .build();
+
+        return ProduitDto.fromEntity(produitRepository.save(donnee));
+    }
+
+    @Override
+    public ProduitDto ModifierProduit(String id, ProduitDto produitDto) {
+        Produit mod = produitRepository.findById(id).orElseThrow();
+        mod.setChecked(produitDto.getChecked());
+        mod.setName(produitDto.getName());
+        mod.setPrice(produitDto.getPrice());
+        return ProduitDto.fromEntity(produitRepository.save(mod));
+    }
+
+    @Override
+    public ProduitDto MajProduit(String id, ProduitDto produitDto) {
+        return null;
+    }
+
+    @Override
+    public void SupprimerProduit(String id) {
+
+    }
+}
